1b02ae33caeedbd5f98c3d5cbd3e70f31e952033
Mayank <134766594+vortex-m@users.noreply.github.com>
2025-05-16 23:19:02 +0530

    Add Syncfusion

--- app/root.tsx
+++ app/root.tsx
@@ -23,6 +23,10 @@ export const links: Route.LinksFunction = () => [
   },
 ];
 
+import { registerLicense } from "@syncfusion/ej2-base";
+
+registerLicense(import.meta.env.DASH_SYNCFUSION_LICENSE);
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
